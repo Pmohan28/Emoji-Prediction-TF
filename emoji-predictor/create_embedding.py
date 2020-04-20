783fb6b95191699400fedad7287670d9ede7a12d
Prateek M <43565512+Pmohan28@users.noreply.github.com>
2020-04-20 13:35:46 +0530

    Update create_embedding.py

--- emoji-predictor/create_embedding.py
+++ emoji-predictor/create_embedding.py
@@ -4,18 +4,31 @@ from keras.models import load_model, Model
 from sklearn.model_selection import train_test_split
 from keras.layers import BatchNormalization
 from keras.preprocessing.text import Tokenizer
-from keras.layers import Dense, Input, Dropout, LSTM, Bidirectional, Activation
+from keras.layers import Dense, Input, Dropout, LSTM, Bidirectional, Activation, GlobalAveragePooling1D, Flatten
 from keras.preprocessing.sequence import pad_sequences
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from keras.regularizers import L1L2
+from keras import callbacks as ck
 from keras.initializers import glorot_uniform
 import numpy as np
 import regex as re
 import os
 import pickle
+import matplotlib.pyplot as plt
 
 
-def read_text_file(file_name):
+# embedding_dim = 100
+
+
+class myCallback(ck.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('accuracy')>0.6):
+      print("\nReached 90% accuracy so cancelling training!")
+      self.model.stop_training = True
+callback = myCallback()
+
+
+def text_read(file_name):
     data_list = []
     with open(file_name, 'r') as f:
         for line in f:
@@ -27,44 +40,54 @@ def read_text_file(file_name):
     return data_list
 
 
-def extract_labels(text_list):
+def labels_read(text_list):
     label_list = []
     text_list = [text_list[i][0].replace('[', '') for i in range(len(text_list))]
     label_list = [list(np.fromstring(text_list[i], dtype=float, sep=' ')) for i in range(len(text_list))]
     return label_list
 
 
-def extract_text_msgs(text_list):
+def msg_read(text_list):
     msg_list = []
     msg_list = [text_list[i][1] for i in range(len(text_list))]
     return msg_list
 
 
-def read_glove_vector(glovefile):
-    with open(glovefile, 'r', encoding='UTF-8') as f:
-        words = set()
-        word_to_vec = {}
+def read_glove_vector():
+    embeddings_index = {}
+    words = set()
+    coefs = {}
+    with open('glove.6B.50d.txt', 'r', encoding='UTF-8') as f:
         for line in f:
-            line = line.strip().split()
-            line[0] = re.sub('[^a-zA-Z]', '', line[0])
-            if len(line[0]) > 0:
-                words.add(line[0])
-                word_to_vec[line[0]] = np.array(line[1:], dtype=np.float64)
-                # print (word_to_vec[line[0]])
+            values = line.strip().split()
+            values[0] = re.sub('[^a-zA-Z]', '', values[0])
+            if len(values[0]) > 0:
+                words.add(values[0])
+                coefs[values[0]] = np.array(values[1:], dtype= np.float64)
         i = 1
-        word_to_index = {}
-        index_to_word = {}
+        word_index = {}
+        index_word = {}
 
         for word in sorted(words):
-            word_to_index[word] = i
-            index_to_word[i] = word
-
+            word_index[word] = i
+            index_word[i] = word
             i = i + 1
+            vocab_size = len(word_index) + 1
+            embed_dim = coefs['word'].shape[0]
+        # print(vocab_size)
+        # print(embed_dim)
 
-    return word_to_index, index_to_word, word_to_vec
+        embeddings_matrix = np.zeros((vocab_size, embed_dim))
+        for word, i in word_index.items():
+            embeddings_matrix[i,:] = coefs[word]
+        embedding_layer = Embedding(vocab_size, embed_dim)
+        embedding_layer.build((None,))
+        embedding_layer.set_weights([embeddings_matrix])
+        print(embeddings_matrix.shape)
+        return embedding_layer
 
 
-def sentences_to_indices(text_arr, word_to_index, max_len):
+def sentences_to_indices(text_arr, word_index, max_len):
     m = text_arr.shape[0]
     X_indices = np.zeros((m, max_len))
     for i in range(m):
@@ -72,79 +95,61 @@ def sentences_to_indices(text_arr, word_to_index, max_len):
 
         j = 0
         for w in sentence_words:
-            X_indices[i, j] = word_to_index[w]
+            X_indices[i, j] = word_index[w]
             j += 1
 
     return X_indices
 
 
-def create_embeddings_layer(word_to_index, word_to_vec):
-    corpus_len = len(word_to_index) + 1
-    embedding_dim = word_to_vec['word'].shape[0]
-    embed_matrix = np.zeros(shape=(corpus_len, embedding_dim))
-
-    for word, index in word_to_index.items():
-        embed_matrix[index, :] = word_to_vec[word]
-
-    embedding_layer = Embedding(corpus_len, embedding_dim)
-
-    embedding_layer.build((None,))
-    embedding_layer.set_weights([embed_matrix])
-    # print(embedding_layer.get_weights()[0][1][3])
-    return embedding_layer
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
 
 
-def create_lstm_model(inputshape, embedding_layer):
+def lstm_model(inputshape, embedding_layer):
     sentences_indices = Input(shape=inputshape, dtype=np.int32)
     embedding_layer = embedding_layer
     embeddings = embedding_layer(sentences_indices)
     X = Bidirectional(LSTM(256, return_sequences=True, recurrent_dropout=0.2, dropout=0.20, bias_regularizer=L1L2(0.01, 0.02)))(embeddings)
+    X = Bidirectional(LSTM(256, return_sequences=True, recurrent_dropout=0.2, dropout=0.20))(X)
     X = BatchNormalization()(X)
     X = Dropout(0.4)(X)
-    X = LSTM(128)(X)
-    X= Dropout(0.40)(X)
+    X = Flatten()(X)
+    X = Dense(128,activation='relu')(X)
+    X = Dropout(0.40)(X)
     X = Dense(7, activation='softmax')(X)
     model = Model(sentences_indices, X)
     return model
 
-
-
-
 if __name__ == "__main__":
-    textlist = read_text_file('data.txt')
-    label_list = extract_labels(textlist)
-    msg_list = extract_text_msgs(textlist)
-    word_to_index, index_to_word, word_to_vec = read_glove_vector('glove.6B.50d.txt')
-    x_train, x_test, y_train, y_test = train_test_split(msg_list, label_list, test_size=0.20, stratify=label_list,
-                                                        random_state=1234)
-    t = Tokenizer(lower=True, filters='')
+    textlist = text_read('data.txt')
+    label_list = labels_read(textlist)
+    msg_list = msg_read(textlist)
+    x_train, x_test, y_train, y_test = train_test_split(msg_list, label_list, test_size=0.20, stratify=label_list,random_state=1234)
+    t = Tokenizer( lower=True, filters='', oov_token="<OOV>")
     t.fit_on_texts(msg_list)
+    embeddings_matrix = read_glove_vector()
     x_train_tokenized = t.texts_to_sequences(x_train)
     x_test_tokenized = t.texts_to_sequences(x_test)
     max_len = 50
     X_train = pad_sequences(x_train_tokenized, padding='post', maxlen=max_len)
     X_test = pad_sequences(x_test_tokenized, padding='post', maxlen=max_len)
-    # if os.path.exists('tokenizer.pickle'):
-    #     os.remove('tokenizer.pickle')
     with open('tokenizer.pickle', 'wb') as tokenizer:
         pickle.dump(t, tokenizer, protocol=pickle.HIGHEST_PROTOCOL)
-    embedding_layer = create_embeddings_layer(word_to_index, word_to_vec)
-    model = create_lstm_model((max_len,), embedding_layer)
+    embedding_layer = read_glove_vector()
+    model = lstm_model((max_len,), embedding_layer)
     print(model.summary())
-    print(np.array(y_train).shape)
-    # callbacks = [
-    #     EarlyStopping(patience=12, verbose=1),
-    #     ReduceLROnPlateau(factor=0.1, patience=5, min_lr=0.000001, verbose=1),
-    #     ModelCheckpoint('./emoji-{epoch:03d}.h5', verbose=1, save_weights_only=True)
-    # ]
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(X_train, np.array(y_train), batch_size=32, epochs=32, validation_data=[X_test, np.array(y_test)])
+    history = model.fit(X_train, np.array(y_train), batch_size=32, epochs=100, validation_data=[X_test, np.array(y_test)], callbacks= [callback])
+    plot_graphs(history, 'accuracy')
+    plot_graphs(history, 'loss')
     model.save('emoji_weights.h5')
     loss, acc = model.evaluate(X_test, np.array(y_test))
-    test_sent = t.texts_to_sequences(['Feeling Happy about the day'])
+    test_sent = t.texts_to_sequences(['Feeling Happy today'])
     test_sent = pad_sequences(test_sent, maxlen=max_len)
     pred = model.predict(test_sent)
     print(np.argmax(pred))
-
-
-
